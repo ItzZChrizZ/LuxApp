7d30a3a1eba6614cb95408670027cb321e1fc1b7
ItzZChrizZ <ugurozkan35@gmail.com>
2022-03-23 23:46:18 +0300

    Firebase widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:luxapp/pages/loading_page.dart';
 import 'package:luxapp/pages/main_page.dart';
 
 void main(List<String> args) {
@@ -10,9 +12,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
+    return MaterialApp(
+      home: LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Scaffold(
+            body: Center(
+              child: Text("Error"),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MainPage();
+        }
+        return const LoadingPage();
+      },
     );
   }
 }
