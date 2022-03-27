91c2e5fc4b388e97ee840eeeb3b84f8e70b28dac
ItzZChrizZ <ugurozkan35@gmail.com>
2022-03-28 00:07:39 +0300

    Order Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:luxapp/pages/loading_page.dart';
-import 'package:luxapp/pages/main_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/landing_page.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -13,32 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       home: LandingPage(),
-    );
-  }
-}
-
-class LandingPage extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-  LandingPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Scaffold(
-            body: Center(
-              child: Text("Error"),
-            ),
-          );
-        }
-        if (snapshot.connectionState == ConnectionState.done) {
-          return const MainPage();
-        }
-        return const LoadingPage();
-      },
     );
   }
 }
---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:luxapp/pages/loading_page.dart';
+import 'package:luxapp/pages/main_page.dart';
+
+class LandingPage extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Scaffold(
+            body: Center(
+              child: Text("Error"),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MainPage();
+        }
+        return const LoadingPage();
+      },
+    );
+  }
+}
